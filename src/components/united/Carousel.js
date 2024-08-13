1bb7bf84237c1758e82c506f6cc48f0ca3ed3346
D <htehran333@gmail.com>
2024-08-13 13:07:46 -0700

    Carousel is Fixed

--- src/components/united/Carousel.js
+++ src/components/united/Carousel.js
@@ -3,14 +3,22 @@ import { Card } from "../Portal";
 import "@splidejs/react-splide/css";
 
 const Carousel = ({ title, products, dark = false }) => {
+  console.log(products.length);
+
   return (
     <section
       className={`paddin ${
         dark ? "bg-slate-800" : "bg-slate-100"
-      } lg:flex items-center py-5`}
+      } items-center p-5`}
     >
-      <div className="flex-fullcenter title lg:w-36 w-full text-center px-8 max-lg:py-5">
-        <h2 className={dark ? "text-slate-200" : ""}>{title}</h2>
+      <div className="flex-fullcenter title w-full text-center px-8 pt-1 pb-5 max-lg:py-5">
+        <h2
+          className={`${
+            dark ? "bg-slate-200 text-slate-800" : "text-slate-200 bg-slate-800"
+          } px-5 py-1`}
+        >
+          {title}
+        </h2>
       </div>
       <div dir="ltr">
         <Splide
@@ -20,10 +28,10 @@ const Carousel = ({ title, products, dark = false }) => {
             arrows: false,
             pagination: false,
             gap: "2em",
-            updateOnMove: true,
+            omitEnd: true,
             breakpoints: {
-              1640: {
-                perPage: 12,
+              1440: {
+                perPage: 4,
               },
               1024: {
                 perPage: 3,
