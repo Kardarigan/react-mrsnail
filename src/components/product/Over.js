5f1bec3b310931dae1f391891017e591817e8273
D <htehran333@gmail.com>
2024-08-18 14:49:16 -0700

    Over is Done

--- src/components/product/Over.js
+++ src/components/product/Over.js
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 import { orderLink } from "../../data/constans";
+import { Tip } from "../Portal";
 
 const Over = ({ product }) => {
   return (
     <div className="max-lg:p-5 max-lg:mt-12 flex flex-col gap-y-12">
+      <h1 className="title-lg">{product.title}</h1>
       <div>
-        <h1 className="title-lg">{product.title}</h1>
-        <h2>دسته‌بندی : {product.category}</h2>
+        <h2>دسته بندی : {product.category}</h2>
+        <h2>برند محصول : {product.brand}</h2>
       </div>
       <div className="">
         <h2 className="title mark mark-dark inline">
           قیمت : {product.price} هزار تومن
         </h2>
-        <div className="flex max-md:flex-col gap-2 mt-5">
+        <p className="label mt-12">
+          اگه تعداد بالا میخوای توی تلگرام پیام بده، در غیر این صورت بزنگ!
+        </p>
+        <div className="flex max-md:flex-col gap-2 mt-2">
           {orderLink.map((button, index) => {
             return (
               <Link
@@ -20,7 +25,7 @@ const Over = ({ product }) => {
                 key={index}
                 className={`button ${index > 0 ? "button-dark" : "button-sky"}`}
               >
-                <div className="flex items-center gap-2">
+                <div className="flex-fullcenter gap-2">
                   <i className={button.icon}></i>
                   {button.label}
                 </div>
